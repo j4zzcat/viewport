bb1cf1644a886cbb969c490dd369a6597ade3937
Sharon Dagan <sharond@totango.com>
2024-06-30 16:50:58 +0300

    Added simple benchmark

--- utils/report_avg_cpu.sh
+++ utils/report_avg_cpu.sh
@@ -1,10 +1,29 @@
+#!/usr/bin/env bash
 
-# RPI
-top_log='/tmp/top.log'
-clean_log='/tmp/top.clean.log'
-eval top -p $(pgrep ffmpeg | xargs | sed -e 's/ / -p /g') -n 30 -b >"$top_log"
-grep <"$top_log" -A5 PID | grep -v '^    PID' | grep -v '^--' | sort | awk '{print $1, $9}' >"$clean_log"
-for pid in $(sort <"$clean_log" | awk '{print $1}' | uniq); do
-  grep <"$clean_log" "$pid" | awk 'BEGIN{s=0}{s=s+$2}END{print '"$pid"', s/NR}'
+sample_process() {
+  local _process_name="$1"
+  local _output_file="$2"
+  local _top_log=$(mktemp)
+
+  case "$(uname)" in
+    Darwin)
+      eval top -pid $(pgrep "$_process_name" | xargs | sed -e 's/ / -pid /g') -l 30 >"$_top_log"
+      grep <"$_top_log" -A5 PID | grep -v '^PID' | grep -v '^--' | sort | awk '{print $1, $3}' >"$_output_file"
+      ;;
+
+    Linux)
+      eval top -p $(pgrep "$_process_name" | xargs | sed -e 's/ / -p /g') -n 30 -b >"$_top_log"
+      grep <"$_top_log" -A5 PID | grep -v '^    PID' | grep -v '^--' | sort | awk '{print $1, $9}' >"$_output_file"
+      ;;
+  esac
+}
+
+# Main
+
+top_log_file=$(mktemp)
+sample_process 'ffmpeg' "$top_log_file"
+for pid in $(sort <"$top_log_file" | awk '{print $1}' | uniq); do
+  grep <"$top_log_file" "$pid" | awk 'BEGIN{s=0}{s=s+$2}END{print '"$pid"', s/NR}'
 done
+
 
