668741a1d4d46e7ade1c488dd0fe08c8b4c5eb61
Sharon Dagan <sharond@totango.com>
2024-07-04 15:28:44 +0300

    Initial login

---
+++ src/streamline-viewport.ts
@@ -0,0 +1,38 @@
+import * as shell from "shelljs";
+import { ProtectApi, ProtectNvrBootstrap } from "unifi-protect";
+import * as util from "node:util";
+
+const USERID = 'viewport-1';
+const PASSWORD = shell.exec('security find-generic-password -l dev-user -a unifi-protect -w', {silent: true}).split('\n')[0];
+
+async function login(ufp: ProtectApi) {
+    // Set a listener to wait for the bootstrap event to occur.
+    ufp.once("bootstrap", (bootstrapJSON: ProtectNvrBootstrap) => {
+
+        // Once we've bootstrapped the Protect controller, output the bootstrap JSON and we're done.
+        process.stdout.write(util.inspect(bootstrapJSON, {
+            colors: true,
+            depth: null,
+            sorted: true
+        }) + "\n", () => process.exit(0));
+    });
+
+    // Login to the Protect controller.
+    if (!(await ufp.login("192.168.4.10", USERID, PASSWORD))) {
+
+        console.log("Invalid login credentials.");
+        process.exit(0);
+    }
+    ;
+
+    // Bootstrap the controller. It will emit a message once it's received the bootstrap JSON, or you can alternatively wait for the promise to resolve.
+    if (!(await ufp.getBootstrap())) {
+
+        console.log("Unable to bootstrap the Protect controller.");
+        util.process.exit(0);
+    }
+}
+
+// Create a new Protect API instance.
+const ufp = new ProtectApi();
+login(ufp);
