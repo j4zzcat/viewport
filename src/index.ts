fcfd156213c57d325dbe6d95f68eb97f5ab9796f
Sharon Dagan <sharond@totango.com>
2024-07-09 02:00:35 +0300

    Removed unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node --no-warnings --import tsx
 
-import {Backend, PluginRegistry, GridLayoutManager, RTSPStreamManager, UnifiStreamManager} from "./backend/backend";
 import {MainCommandLine} from "./frontend/cli";
 
 const commandLine: MainCommandLine = new MainCommandLine();
