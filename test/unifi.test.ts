c394c127fd5601195b05ea101d32864d880098e5
Sharon Dagan <sharond@totango.com>
2024-07-09 23:50:43 +0300

    More tests

--- test/unifi.test.ts
+++ test/unifi.test.ts
@@ -1,9 +1,16 @@
 import {describe, expect, test} from '@jest/globals';
 import {UnifiStreamsManager} from "../src/backend/unifi";
 
-describe('testing index file', () => {
-    test('empty string should result in zero', () => {
+describe('Testing UnifiVideoProvider', () => {
+    test('Valid URL can be handled', () => {
         let usm = new UnifiStreamsManager();
         expect(usm.canHandle(new URL('unifi://u:p@host/camera'))).toBe(true);
     });
+
+    test('Invalid URL cannot be handled', () => {
+        let usm = new UnifiStreamsManager();
+        expect(usm.canHandle(new URL('xunifi://u:p@host/camera'))).toBe(false);
+        expect(usm.canHandle(new URL('unifi://u:p@host/xcamera'))).toBe(false);
+    });
+
 });
