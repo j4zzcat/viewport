309c5e9eabd399ec8e60454417e1fea27ecaf7e2
Sharon Dagan <sharond@totango.com>
2024-07-16 02:13:33 +0300

    Initial commit

---
+++ src/controller/lib/controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "controller/version"
+
+module Controller
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+puts "Hello world!"
