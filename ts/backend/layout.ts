4cc58bbd9551461f82b23ecad41de6c1513fe0a5
Sharon Dagan <sharond@totango.com>
2024-07-16 01:36:59 +0300

    Cleanup

--- ts/backend/layout.ts
+++
@@ -1,9 +0,0 @@
-import {ILayoutManager} from "./backend";
-import {BasePlugin} from "../utils/plugin";
-
-export class GridLayoutManager extends BasePlugin implements ILayoutManager {
-    constructor() {
-        super('grid');
-    }
-}
-
--- ts/index.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node --no-warnings --import tsx
-
-import {context} from "./context";
-
-const commandLine = context.createMainCommandLine();
-commandLine.executeAsync();
