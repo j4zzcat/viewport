49429fa38887021e19f030a28ba0e883215f0d26
Sharon Dagan <sharond@totango.com>
2024-07-16 00:11:56 +0300

    Docs

--- src/simple-player/src/simple-player.ts
+++ src/simple-player/src/simple-player.ts
@@ -16,6 +16,20 @@
  * This software. If not, see <https://www.gnu.org/licenses/>.
  */
 
+/**
+ * Simple Player - plays the video feed of a Unifi camera, with low latency.
+ * The player receives the stream from the Simple Reflector, which handles
+ * the mechanics of getting the stream from Unifi Protect, and then sends it
+ * over WebSocket to this player. The video is then presented using MSE.
+ *
+ * Usually, the video is formatted as an H.264 fMP4 stream, with avc1.4d4032
+ * codec for video and mp4a.40.2 codec for audio. This format is natively
+ * supported by MSE, so no further transcoding is necessary, just careful
+ * management of the SourceBuffer object.
+ *
+ * Under normal circumstances, the player achieves latency of 0.5 - 1 second.
+ */
+
 import {Queue} from "typescript-collections";
 
 export class SimplePlayer {
@@ -31,7 +45,7 @@ export class SimplePlayer {
     private CLEANUP_INTERVAL_SECONDS = 20;
     private KEEP_VIDEO_SECONDS = 10
 
-    constructor(videoElementId, url) {
+    constructor(videoElementId: string, url: string) {
         this._videoElementId = videoElementId;
         this._url = url;
 
@@ -71,24 +85,8 @@ export class SimplePlayer {
         this._ws.onmessage = (event) => {
             messageCount++;
 
-            /*
-             * Do a housekeeping cycle every HOUSE_KEEPING_INTERVAL_MESSAGES messages.
-             */
-            if(messageCount % this.HOUSEKEEPING_INTERVAL_MESSAGES == 0) {
-                this.log(`Starting housekeeping cycle, messageCount: ${messageCount}`);
-
-                /*
-                 * Request a cleaning cycle every CLEANUP_INTERVAL_SECONDS.
-                 * Without a cleanup, the SourceBuffer will eventually overflow.
-                 */
-                if (cleanup == 0) {
-                    if((Date.now() - lastCleanup) > this.CLEANUP_INTERVAL_SECONDS * 1000) {
-                        cleanup = 1;
-                    }
-                }
-            }
-
             if (messageCount == 1) {
+                this.log("Processing message: 1");
 
                 /*
                  * The first message from the server contains the codecs of this stream, which
@@ -100,15 +98,17 @@ export class SimplePlayer {
 
                 this._mimeCodecs = `video/mp4; codecs="${event.data}"`
 
-                this.log(`mimeCodec: ${this._mimeCodecs}`);
+                this.log(`Got mimeCodec: ${this._mimeCodecs}`);
                 if (!MediaSource.isTypeSupported(this._mimeCodecs)) {
                     throw new Error(`Mime Codec not supported: ${this._mimeCodecs}`);
                 }
 
+                this.log("Allocating SourceBuffer and Queue");
                 this._sourceBuffer = this._mediaSource.addSourceBuffer(this._mimeCodecs);
                 this._queue = new Queue<Uint8Array>();
 
             } else if(messageCount == 2) {
+                this.log("Processing message: 2");
 
                 /*
                  * The second message from the server is the first message of the stream.
@@ -116,6 +116,7 @@ export class SimplePlayer {
                  * all other messages.
                  */
 
+                this.log("Appending Init segment");
                 const data = new Uint8Array(event.data);
                 this._sourceBuffer.appendBuffer(data);
 
@@ -131,6 +132,32 @@ export class SimplePlayer {
                  * avoiding any lag accumulation.
                  */
 
+                /*
+                 * Do a housekeeping cycle every HOUSE_KEEPING_INTERVAL_MESSAGES messages.
+                 */
+                if(messageCount % this.HOUSEKEEPING_INTERVAL_MESSAGES == 0) {
+                    this.log(`Starting housekeeping cycle, messageCount: ${messageCount}`);
+
+                    /*
+                     * Request a cleaning cycle every CLEANUP_INTERVAL_SECONDS.
+                     * Without a cleanup, the SourceBuffer will eventually overflow.
+                     */
+                    if (cleanup == 0) {
+                        if((Date.now() - lastCleanup) > this.CLEANUP_INTERVAL_SECONDS * 1000) {
+                            lastCleanup = Date.now();
+                            cleanup = 1;
+                        }
+                    }
+                }
+
+                /*
+                 * Handle the H.264 fMP4 stream. Ideally, when a message arrives it can be
+                 * appended to the SourceBuffer right away. However, there are times when the
+                 * SourceBuffer is not yet ready, and so the message is enqueued. Whenever the
+                 * SourceBuffer can be updated, all queued messages are appended to the
+                 * SourceBuffer, effectively cleaning up the queue and avoiding any lag
+                 * accumulation.
+                 */
                 const data = new Uint8Array(event.data);
                 this._queue.enqueue(data);
 
