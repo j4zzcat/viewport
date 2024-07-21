85c3b8eb1f96f3c154336f565c84321f3b5ecf6a
Sharon Dagan <sharond@totango.com>
2024-07-22 01:49:31 +0300

    Revert "Fixed underflow when regaining focus"
    This reverts commit c0ce02dbbe2d82ad98adf45b6a9a5c9ec50c3556.

--- src/player/src/simple-player.ts
+++ src/player/src/simple-player.ts
@@ -35,7 +35,6 @@ import {Queue} from "typescript-collections";
 export class SimplePlayer {
     private readonly _videoElementId: string;
     private readonly _url: string;
-    private _videoElement: HTMLVideoElement;
     private _mimeCodecs: string;
     private _ws: WebSocket;
     private _mediaSource: MediaSource;
@@ -55,43 +54,20 @@ export class SimplePlayer {
     }
 
     private initialize() {
-        this._videoElement = document.getElementById(this._videoElementId) as HTMLVideoElement;
-        if(this._videoElement == undefined) {
+        const videoElement: HTMLVideoElement = document.getElementById(this._videoElementId) as HTMLVideoElement;
+        if(videoElement == undefined) {
             throw Error(`Video element '${this._videoElementId}' not found`);
         }
-
-        // this._videoElement.pause();
-        this._videoElement.addEventListener("error", (event) => {
-            this.log(`Error, onerror: ${event}`);
-        });
-
-        this._videoElement.addEventListener("stalled", (event) => {
-            this.log(`Error, onstalled: ${event}`);
-        });
-
 
         if (!("MediaSource" in window)) {
             throw new Error("MediaSource API is not available in your browser.");
         }
         this._mediaSource = new MediaSource();
 
-        this._videoElement.src = URL.createObjectURL(this._mediaSource);
+        videoElement.src = URL.createObjectURL(this._mediaSource);
         this._mediaSource.addEventListener("sourceopen", this.onSourceOpen);
         this._mediaSource.addEventListener("sourceended", this.onSourceEnded);
         this._mediaSource.addEventListener("sourceclose", this.onSourceClose);
-
-        document.addEventListener('visibilitychange', () => {
-            if (document.hidden) {
-                this.log("Visibility changed: Hidden")
-                // this._videoElement.pause();
-                // this._ws.close();
-            } else {
-
-                this.log("Visibility changed: Focus")
-                this._videoElement.currentTime = this._sourceBuffer.buffered.end(0);
-                // this.restartPlayback();
-            }
-        });
     }
 
     private onSourceOpen = () => {
@@ -130,145 +106,6 @@ export class SimplePlayer {
                 this.log("Allocating SourceBuffer and Queue");
                 this._sourceBuffer = this._mediaSource.addSourceBuffer(this._mimeCodecs);
                 this._queue = new Queue<Uint8Array>();
-                return
-
-            }
-
-            /*
-             * All other messages are handled here. All of them are segments of the
-             * H.264 fMP4 stream.
-             */
-
-            /*
-             * Do a housekeeping cycle every HOUSE_KEEPING_INTERVAL_MESSAGES messages.
-             */
-            if(messageCount % this.HOUSEKEEPING_INTERVAL_MESSAGES == 0) {
-                this.log("Starting housekeeping cycle");
-                this.log(`messageCount: ${messageCount}, queue.size: ${this._queue.size()}`);
-
-                /*
-                 * Request a cleaning cycle every CLEANUP_INTERVAL_SECONDS.
-                 * Without a cleanup, the SourceBuffer will eventually overflow.
-                 */
-                if (cleanup == 0) {
-                    if((Date.now() - lastCleanup) > this.CLEANUP_INTERVAL_SECONDS * 1000) {
-                        lastCleanup = Date.now();
-                        cleanup = 1;
-                    }
-                }
-            }
-
-            /*
-             * Handle the H.264 fMP4 stream. Ideally, when a message arrives it can be
-             * appended to the SourceBuffer right away. However, there are times when the
-             * SourceBuffer is not yet ready, and so the message is enqueued to be later
-             * appended to the SourceBuffer. Whenever the SourceBuffer can be updated,
-             * all queued messages are appended at once, effectively cleaning up the queue
-             * and avoiding any lag accumulation.
-             */
-            const data = new Uint8Array(event.data);
-            this._queue.enqueue(data);
-
-            if (this._sourceBuffer.updating == false) {
-
-                if (cleanup > 0) {
-
-                    /*
-                     * A cleanup cycle was requested, start it now. Cleanup can
-                     * proceed only if the SourceBuffer is ready (i.e., not updating).
-                     * If successful, the SourceBuffer is reduced, leaving in it only
-                     * the last KEEP_VIDEO_SECONDS seconds of video.
-                     */
-
-                    if(cleanup == 1) {
-                        this.log("Starting cleanup cycle");
-                    } else {
-                        this.log(`Retrying cleanup cycle, this is attempt: ${cleanup}`)
-                    }
-
-                    try {
-                        for(let i = 0; i < this._sourceBuffer.buffered.length; i++) {
-                            this.log(`SourceBuffer.buffered[${i}] start: ${this._sourceBuffer.buffered.start(i)}, end: ${this._sourceBuffer.buffered.end(i)}`);
-
-                            // this._sourceBuffer.remove(0, Math.max(end - this.KEEP_VIDEO_SECONDS, 1));
-                            this.log("SourceBuffer cleaned");
-                        }
-                        cleanup = 0;
-                        return;
-
-                    } catch (e) {
-                        this.log(`Failed to clean up SourceBuffer: ${e}`);
-                        cleanup++;
-                    }
-                }
-
-                if(messageCount >= 2) {
-                    const data = new Uint8Array(event.data);
-                    this._sourceBuffer.appendBuffer(this._queue.dequeue());
-                }
-
-                //let allWaitingMessages = this.dehydrateQueue(this._queue);
-                //this._sourceBuffer.appendBuffer(allWaitingMessages);
-            }
-        }
-    }
-
-    onupdateend = () => {
-        this.log("SourceBuffer.onupdateend")
-        this._sourceBuffer.appendBuffer(this._queue.dequeue());
-        // this._sourceBuffer.onupdateend = this.onupdateend;
-    }
-
-    private restartPlayback = () => {
-        this._ws = new WebSocket(this._url);
-        this._ws.binaryType = "arraybuffer";
-
-        this._ws.onopen = () => {
-            this.log("WebSocket connection opened.");
-        }
-
-        let messageCount = 0;
-        let cleanup = 0;
-        let lastCleanup = Date.now();
-
-        this._ws.onmessage = (event) => {
-            messageCount++;
-
-            if (messageCount == 1) {
-                this.log("Processing message: 1");
-
-                /*
-                 * The first message from the server contains the codecs of this stream, which
-                 * are usually avc1.4d4032 for video and mp4a.40.2 for audio. All other messages
-                 * are fragments of the H.264 fMP4 stream of the camera. Essentially, this is
-                 * the initialization section of the engine where the SourceBuffer and Queue
-                 * are allocated.
-                 */
-
-                this._mimeCodecs = `video/mp4; codecs="${event.data}"`
-
-                this.log(`Got mimeCodec: ${this._mimeCodecs}`);
-                if (!MediaSource.isTypeSupported(this._mimeCodecs)) {
-                    throw new Error(`Mime Codec not supported: ${this._mimeCodecs}`);
-                }
-
-                this.log("Allocating SourceBuffer and Queue");
-                if(!this._sourceBuffer) {
-                    this._sourceBuffer = this._mediaSource.addSourceBuffer(this._mimeCodecs);
-                    this._queue = new Queue<Uint8Array>();
-
-                } else {
-                    while(true) {
-                        this._sourceBuffer.abort();
-                        try {
-                            this._sourceBuffer.remove(0, this._sourceBuffer.buffered.end(0) - 1);
-                        } catch (e) {
-                            this.log(`Error: ${e}`);
-                            continue;
-                        }
-                        break;
-                    }
-                }
 
             } else if(messageCount == 2) {
                 this.log("Processing message: 2");
@@ -282,7 +119,6 @@ export class SimplePlayer {
                 this.log("Appending Init segment");
                 const data = new Uint8Array(event.data);
                 this._sourceBuffer.appendBuffer(data);
-                this._videoElement.play();
 
             } else {
 
@@ -342,7 +178,7 @@ export class SimplePlayer {
                             let end = this._sourceBuffer.buffered.end(0);
                             this.log(`Cleaning up SourceBuffer, end: ${end}`);
 
-                            this._sourceBuffer.remove(0, Math.max(end - this.KEEP_VIDEO_SECONDS, 1));
+                            this._sourceBuffer.remove(0, end - this.KEEP_VIDEO_SECONDS);
                             this.log("SourceBuffer cleaned");
 
                             cleanup = 0;
@@ -369,7 +205,7 @@ export class SimplePlayer {
         const buffers: Uint8Array[] = [];
 
         let totalLength = 0;
-        while (!this._queue.isEmpty()) {
+        while (this._queue.size() > 4) {
             const array = this._queue.dequeue();
             buffers.push(array);
             totalLength += array.length;
