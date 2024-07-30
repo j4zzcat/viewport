66299d37e9c676323f80bb584b2d8eec62ae0040
Sharon Dagan <sharond@totango.com>
2024-07-30 16:11:10 +0300

    Simplification

--- build/build.sh
+++ build/build.sh
@@ -1,54 +1,18 @@
 #! /bin/bash
 
-usage() {
-    echo "usage: $(basename $0) [-a]"
-}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"/..
 
-error_message_file=$(mktemp /tmp/XXXXX)
-valid_args=$(getopt ha "$@" 2>"$error_message_file")
-if [[ $? -ne 0 ]]; then
-    echo "error. $(cat "$error_message_file")" 2>/dev/stderr
-    usage && exit 129
-fi
+IMAGE_TAG='1.1.0'
+IMAGE_NAME='j4zzcat/viewport'
 
-eval set -- "$valid_args"
-while :; do
-  case "$1" in
-    -a | --all)
-      shift 1
-      ALL='true'
-      ;;
+docker buildx create --name container --driver=docker-container 2>/dev/null
 
-    -h | --help)
-      shift 1
-      usage && exit 0
-      ;;
-
-    --) shift;
-      break
-      ;;
-  esac
-done
-
-image_tag=$(git describe --tags 2>/dev/null || echo 'latest')
-image_name='j4zzcat/viewport'
-
-if [[ "$ALL" == 'true' ]]; then
-  docker buildx create --name container --driver=docker-container 2>/dev/null
-
-  docker buildx build \
-    --tag "$image_name:$image_tag" \
-    --tag "$image_name:latest" \
-    --platform linux/arm64,linux/amd64 \
-    --builder container \
-    --push \
-    -f docker/Dockerfile .
-
-else
-  # Default, no hassle build
-  docker buildx build \
-    -t "$image_name:$image_tag" \
-    -t "$image_name:latest" \
-    -f docker/Dockerfile .
-fi
+docker buildx build \
+  --tag "$IMAGE_NAME:$IMAGE_TAG" \
+  --tag "$IMAGE_NAME:latest" \
+  --platform linux/arm64,linux/amd64 \
+  --builder container \
+  --push \
+  -f build/Dockerfile .
 
